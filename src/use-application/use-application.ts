1b5b5e4bfba6a4a5673acc2332322e10779be3c9
Adrien Poly <adrienpoly@gmail.com>
2020-07-30 19:21:03 +0200

    use defineProperty for geters instead of assign

--- src/use-application/use-application.ts
+++ src/use-application/use-application.ts
@@ -1,6 +1,18 @@
 import { Controller } from 'stimulus'
 
 export const useApplication = (controller: Controller) => {
+  Object.defineProperty(controller, 'isPreview', {
+    get(): boolean {
+      return document.documentElement.hasAttribute('data-turbolinks-preview')
+    },
+  })
+
+  Object.defineProperty(controller, 'csrfToken', {
+    get(): boolean {
+      return this.metaValue('csrf-token')
+    },
+  })
+
   Object.assign(controller, {
     dispatch(
       eventName: String,
@@ -14,12 +26,6 @@ export const useApplication = (controller: Controller) => {
     metaValue(name: string) {
       const element = document.head.querySelector(`meta[name="${name}"]`)
       return element && element.getAttribute('content')
-    },
-    get isPreview(): boolean {
-      return document.documentElement.hasAttribute('data-turbolinks-preview')
-    },
-    get csrfToken() {
-      return this.metaValue('csrf-token')
     },
   })
 }
