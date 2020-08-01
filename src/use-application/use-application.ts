40af98868287b7bdc79548d7b8e7b6f93455d93c
Adrien Poly <adrienpoly@gmail.com>
2020-08-01 10:51:18 +0200

    add comments

--- src/use-application/use-application.ts
+++ src/use-application/use-application.ts
@@ -1,12 +1,14 @@
 import { Controller } from 'stimulus'
 
 export const useApplication = (controller: Controller) => {
+  // getter to detect Turbolink preview
   Object.defineProperty(controller, 'isPreview', {
     get(): boolean {
       return document.documentElement.hasAttribute('data-turbolinks-preview')
     },
   })
 
+  // getter to get the csrf token
   Object.defineProperty(controller, 'csrfToken', {
     get(): boolean {
       return this.metaValue('csrf-token')
@@ -19,7 +21,11 @@ export const useApplication = (controller: Controller) => {
       { target = controller.element, detail = {}, bubbles = true, cancelable = true } = {},
     ): CustomEvent {
       const type = `${controller.identifier}:${eventName}`
-      const event = new CustomEvent(type, { detail, bubbles, cancelable })
+      const event = new CustomEvent(type, {
+        detail,
+        bubbles,
+        cancelable,
+      })
       target.dispatchEvent(event)
       return event
     },
