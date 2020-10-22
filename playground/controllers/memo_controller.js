c578a52c03763fc4aefd4461bc59bdb0c5def4fa
Julian Rubisch <julian@julianrubisch.at>
2020-10-22 15:27:59 +0200

    Update example
    use console.time for simple profiling

--- playground/controllers/memo_controller.js
+++ playground/controllers/memo_controller.js
@@ -2,32 +2,22 @@ import { Controller } from 'stimulus'
 import { useMemo } from 'stimulus-use'
 
 export default class extends Controller {
-  static memos = ['a', 'b']
+  static memos = ['normalizedText']
+  static targets = ['longText']
 
   connect() {
     useMemo(this)
 
-    console.log(this.a)
-    console.log(this.b)
-    console.log(this.c)
+    console.time('first call')
+    this.normalizedText // process the text and cache the result
+    console.timeEnd('first call')
 
-    console.log(this.a)
-    console.log(this.b)
-    console.log(this.c)
+    console.time('memoized call')
+    this.normalizedText // gets the results from the cache
+    console.timeEnd('memoized call')
   }
 
-  get a() {
-    console.log('getting a')
-    return 1
-  }
-
-  get b() {
-    console.log('getting b')
-    return 2
-  }
-
-  get c() {
-    console.log('getting c')
-    return 3
+  get normalizedText() {
+    return this.longTextTarget.textContent.normalize('NFD')
   }
 }
