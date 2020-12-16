69db1ba717e7958a6a4d987cacbd1a71bedba4b8
Adrien Poly <adrienpoly@gmail.com>
2020-12-16 07:49:56 +0100

    sets debug and logger options as optional

--- src/stimulus_use.ts
+++ src/stimulus_use.ts
@@ -2,8 +2,8 @@ import { Controller } from 'stimulus'
 import { Logger } from "./logger"
 
 export interface StimulusUseOptions {
-  debug: boolean
-  logger: Logger
+  debug?: boolean
+  logger?: Logger
 }
 
 const defaultOptions = {
@@ -20,7 +20,7 @@ export class StimulusUse {
   logger: Logger
   controllerId: string | undefined
 
-  constructor(controller: Controller, options?: StimulusUseOptions) {
+  constructor(controller: Controller, options: StimulusUseOptions = {}) {
     this.debug = options?.debug || (controller.application as any).stimulusUseDebug || defaultOptions.debug
     this.logger = options?.logger || defaultOptions.logger
     this.controller = controller
