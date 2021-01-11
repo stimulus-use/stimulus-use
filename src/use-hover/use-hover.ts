8315914e5c7dc362b5d379628c44d6f0ca3f51ca
Sub-Xaero <Sub-Xaero@users.noreply.github.com>
2021-01-11 22:57:35 +0000

    Convert useHover to use new class-based API

--- src/use-hover/use-hover.ts
+++ src/use-hover/use-hover.ts
@@ -1,40 +1,56 @@
-import { method } from '../support/index'
+import { StimulusUse, StimulusUseOptions } from '../stimulus_use'
+import { method } from '../support'
 import { HoverController } from './hover-controller'
 
-export interface HoverOptions {
+export interface HoverOptions extends StimulusUseOptions {
   element?: Element
 }
 
-export const useHover = (controller: HoverController, options: HoverOptions = {}) => {
-  const targetElement: Element = options?.element || controller.element
+export class UseHover extends StimulusUse {
+  controller: HoverController
+  targetElement: Element
 
-  const onEnter = () => {
-    controller.mouseEnter && method(controller, 'mouseEnter').call(controller)
-  }
-  const onLeave = () => {
-    controller.mouseLeave && method(controller, 'mouseLeave').call(controller)
-  }
-
-  const controllerDisconnect = controller.disconnect.bind(controller)
-
-  const observe = () => {
-    targetElement.addEventListener('mouseenter', onEnter)
-    targetElement.addEventListener('mouseleave', onLeave)
+  constructor(controller: HoverController, options: HoverOptions = {}) {
+    super(controller, options)
+    this.targetElement = options?.element || controller.element
+    this.controller = controller
+    this.enhanceController()
+    this.observe()
   }
 
-  const unobserve = () => {
-    targetElement.removeEventListener('mouseenter', onEnter)
-    targetElement.removeEventListener('mouseleave', onLeave)
+  observe = () => {
+    this.targetElement.addEventListener('mouseenter', this.onEnter)
+    this.targetElement.addEventListener('mouseleave', this.onLeave)
   }
 
-  Object.assign(controller, {
-    disconnect() {
-      unobserve()
+  unobserve = () => {
+    this.targetElement.removeEventListener('mouseenter', this.onEnter)
+    this.targetElement.removeEventListener('mouseleave', this.onLeave)
+  }
+
+  private onEnter = () => {
+    this.controller.mouseEnter && method(this.controller, 'mouseEnter').call(this.controller)
+    this.log('mouseEnter', { hover: true })
+  }
+
+  private onLeave = () => {
+    this.controller.mouseLeave && method(this.controller, 'mouseLeave').call(this.controller)
+    this.log('mouseLeave', { hover: false })
+  }
+
+  private enhanceController() {
+    const controllerDisconnect = this.controller.disconnect.bind(this.controller)
+
+    const disconnect = () => {
+      this.unobserve()
       controllerDisconnect()
     }
-  })
 
-  observe()
+    Object.assign(this.controller, { disconnect })
+  }
+}
 
-  return [observe, unobserve] as const
+export const useHover = (controller: HoverController, options: HoverOptions = {}) => {
+  const observer = new UseHover(controller, options)
+  return [observer.observe, observer.unobserve] as const
 }
