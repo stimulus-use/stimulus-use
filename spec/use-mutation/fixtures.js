13d1f88576bb68d8c4ed6261d00ce4e52f4ed8f5
Adrien Poly <adrienpoly@gmail.com>
2021-01-14 22:37:30 +0100

    add basic test for useMutation

---
+++ spec/use-mutation/fixtures.js
@@ -0,0 +1,7 @@
+export const fixtureBase = `
+  <div data-controller="mutation" id="controller">
+    <button id="button"></button>
+    <input id="input" type="text"></input>
+  </div>
+  <div id="outer"></div>
+`
---
+++ spec/use-mutation/use-mutation_spec.js
@@ -0,0 +1,84 @@
+import { Application } from 'stimulus'
+import { nextFrame, TestLogger, remove } from '../helpers'
+import { expect } from 'chai'
+import LogController from './log_controller'
+import UseLogController from './use_log_controller'
+import { fixtureBase } from './fixtures'
+
+const controllers = [
+  {
+    type: 'controller',
+    controller: LogController
+  },
+  {
+    type: 'mixin',
+    controller: UseLogController
+  }
+]
+
+const scenarios = [
+  // {
+  //   name: 'default options',
+  //   fixture: fixtureBase,
+  //   options: {},
+  //   answers: {}
+  // },
+  {
+    name: 'childList: true',
+    fixture: fixtureBase,
+    options: { childList: true },
+    answers: {}
+  }
+]
+
+scenarios.forEach(scenario => {
+  controllers.forEach(Controller => {
+    describe(`useMutation tests scenario : ${scenario.name} controller type ${Controller.type}`, function () {
+      let application
+      let testLogger
+      beforeEach('initialize controller', async function () {
+        application = Application.start()
+        testLogger = new TestLogger()
+        application.testLogger = testLogger
+        application.options = scenario.options
+        fixture.set(scenario.fixture)
+        application.register('mutation', Controller.controller)
+        await nextFrame()
+      })
+
+      after('stop application', async function () {
+        await application.stop()
+        await nextFrame()
+      })
+
+      describe('initiale state', function () {
+        it('connects but does not not call mutate', async function () {
+          await nextFrame()
+          expect(testLogger.eventsFilter({ event: ['connect'] }).length).to.equal(1)
+          expect(testLogger.eventsFilter({ event: ['mutate'] }).length).to.equal(0)
+        })
+      })
+
+      describe('remove an element inside the controller', function () {
+        it('calls mutate', async function () {
+          await remove('#input')
+          expect(testLogger.eventsFilter({ event: ['entry'] }).length).to.equal(1)
+        })
+      })
+
+      describe('remove an element outside the controller', function () {
+        it('does not calls mutate', async function () {
+          await remove('#outer')
+          expect(testLogger.eventsFilter({ event: ['entry'] }).length).to.equal(0)
+        })
+      })
+
+      describe(`remove the element`, async function () {
+        it('fires disconnect preserving `this` context', async function () {
+          await remove('#controller')
+          expect(testLogger.eventsFilter({ id: ['controller'], event: ['disconnect'] }).length).to.equal(1)
+        })
+      })
+    })
+  })
+})
