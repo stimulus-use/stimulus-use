9262cfc82def458e935174c5218ba01f52b016ef
Adrien Poly <adrienpoly@gmail.com>
2021-03-04 09:51:07 +0100

    update playground

--- playground/controllers/transition_controller.js
+++ playground/controllers/transition_controller.js
@@ -5,12 +5,13 @@ export default class extends Controller {
   connect() {
     useTransition(this, {
       enter: 'transform opacity-0 scale-50',
-      enterActive: 'transition ease-in duration-1000',
-      enterTo: 'transform opacity-100 scale-100',
+      enterActive: 'transition ease-in duration-300',
+      enterTo: 'transform opacity-100 scale-100 visible',
       leave: 'transform opacity-100 scale-100',
-      leaveActive: 'transition ease-out duration-1000',
+      leaveActive: 'transition ease-out duration-300',
       leaveTo: 'transform opacity-0 scale-50',
-      hiddenClass: true
+      removeToClasses: false,
+      hiddenClass: 'opacity-0'
     })
   }
 }
