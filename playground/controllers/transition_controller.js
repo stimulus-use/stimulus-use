2ddf5211cd9c9f49cb62291b50bdacce01068fd9
Adrien Poly <adrienpoly@gmail.com>
2021-03-04 01:36:12 +0100

    update playground

--- playground/controllers/transition_controller.js
+++ playground/controllers/transition_controller.js
@@ -4,13 +4,13 @@ import { useTransition } from 'stimulus-use'
 export default class extends Controller {
   connect() {
     useTransition(this, {
-      enter: 'transition ease-out duration-700',
-      enterActive: 'transform opacity-0 scale-0',
-      enterTo: 'transform opacity-1 scale-100',
-      leave: 'transition ease-in duration-100',
-      leaveActive: 'transform opacity-100 scale-100',
-      leaveTo: 'transform opacity-0 scale-0',
-      hiddenClass: false
+      enter: 'transform opacity-0 scale-50',
+      enterActive: 'transition ease-in duration-1000',
+      enterTo: 'transform opacity-100 scale-100',
+      leave: 'transform opacity-100 scale-100',
+      leaveActive: 'transition ease-out duration-1000',
+      leaveTo: 'transform opacity-0 scale-50',
+      hiddenClass: true
     })
   }
 }
