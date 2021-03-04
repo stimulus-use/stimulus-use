69d6389d14231818e8fa571b927b086a861460aa
Adrien Poly <adrienpoly@gmail.com>
2021-03-04 09:50:57 +0100

    fix enter/leave behavior

--- src/use-transition/use-transition.ts
+++ src/use-transition/use-transition.ts
@@ -10,6 +10,9 @@ export interface TransitionOptions {
   leaveActive?: string
   leaveTo?: string
   hiddenClass?: string
+  leaveAfter?: number
+  preserveOriginalClass?: boolean
+  removeToClasses?: boolean
 }
 
 const alpineNames: object = {
@@ -23,7 +26,9 @@ const alpineNames: object = {
 
 const defaultOptions = {
   transitioned: false,
-  hiddenClass: "hidden"
+  hiddenClass: "hidden",
+  preserveOriginalClass: true,
+  removeToClasses: true
 }
 
 export const useTransition = (controller: TransitionComposableController, options: TransitionOptions = {}) => {
@@ -40,7 +45,9 @@ export const useTransition = (controller: TransitionComposableController, option
   if (!((targetElement instanceof HTMLElement) || (targetElement instanceof SVGElement))) return
   const dataset = targetElement.dataset
 
-  const { transitioned, hiddenClass } = Object.assign(defaultOptions, options)
+  const leaveAfter = parseInt(dataset.leaveAfter || "") || options.leaveAfter || 0
+
+  const { transitioned, hiddenClass, preserveOriginalClass, removeToClasses } = Object.assign(defaultOptions, options)
 
   const controllerEnter = controller.enter?.bind(controller)
   const controllerLeave = controller.leave?.bind(controller)
@@ -52,16 +59,26 @@ export const useTransition = (controller: TransitionComposableController, option
     controller.transitioned = true
     controllerEnter && controllerEnter(event)
 
-    const enterClass = getAttribute("enter", options, dataset)
-    const enterActiveClass = getAttribute("enterActive", options, dataset)
-    const enterToClass = getAttribute("enterTo", options, dataset)
+    const enterClasses = getAttribute("enter", options, dataset)
+    const enterActiveClasses = getAttribute("enterActive", options, dataset)
+    const enterToClasses = getAttribute("enterTo", options, dataset)
+    const leaveToClasses = getAttribute("leaveTo", options, dataset)
 
     if (!!hiddenClass) {
       targetElement.classList.remove(hiddenClass)
     }
 
-    await transition(targetElement, enterClass, enterActiveClass, enterToClass)
+    if (!removeToClasses) {
+      removeClasses(targetElement, leaveToClasses)
+    }
+    await transition(targetElement, enterClasses, enterActiveClasses, enterToClasses, hiddenClass, preserveOriginalClass, removeToClasses)
 
+    if (leaveAfter > 0) {
+      setTimeout(() => {
+        leave(event)
+        console.log("after");
+      }, leaveAfter)
+    }
   }
 
   async function leave(event?: Event) {
@@ -69,11 +86,16 @@ export const useTransition = (controller: TransitionComposableController, option
     controller.transitioned = false
     controllerLeave && controllerLeave(event)
 
-    const leaveClass = getAttribute("leave", options, dataset)
-    const leaveActiveClass = getAttribute("leaveActive", options, dataset)
-    const leaveToClass = getAttribute("leaveTo", options, dataset)
+    const leaveClasses = getAttribute("leave", options, dataset)
+    const leaveActiveClasses = getAttribute("leaveActive", options, dataset)
+    const leaveToClasses = getAttribute("leaveTo", options, dataset)
+    const enterToClasses = getAttribute("enterTo", options, dataset)
 
-    await transition(targetElement, leaveClass, leaveActiveClass, leaveToClass)
+    if (!removeToClasses) {
+      removeClasses(targetElement, enterToClasses)
+    }
+
+    await transition(targetElement, leaveClasses, leaveActiveClasses, leaveToClasses, hiddenClass, preserveOriginalClass, removeToClasses)
 
     if (!!hiddenClass) {
       targetElement.classList.add(hiddenClass)
@@ -90,17 +112,45 @@ export const useTransition = (controller: TransitionComposableController, option
     }
   }
 
-  async function transition(element: Element, initialClass: string[], activeClass: string[], toClass: string[]) {
-    element.classList.add(...initialClass);
-    element.classList.add(...activeClass);
-    await nextFrame();
+  async function transition(element: Element, initialClasses: string[], activeClasses: string[], endClasses: string[], hiddenClass: string, preserveOriginalClass: boolean, removeEndClasses: boolean) {
 
-    element.classList.remove(...activeClass);
-    element.classList.add(...toClass);
+    // if there's any overlap between the current set of classes and initialClasses/activeClasses/endClasses,
+    // we should remove them before we start and add them back at the end
+    const stashedClasses: string[] = []
+    if (preserveOriginalClass) {
+      initialClasses.forEach(cls => element.classList.contains(cls) && cls !== hiddenClass && stashedClasses.push(cls))
+      activeClasses.forEach(cls => element.classList.contains(cls) && cls !== hiddenClass && stashedClasses.push(cls))
+      endClasses.forEach(cls => element.classList.contains(cls) && cls !== hiddenClass && stashedClasses.push(cls))
+    }
 
+
+    // Add initial class before element start transition
+    addClasses(element, initialClasses);
+
+    // remove the overlapping classes
+    removeClasses(element, stashedClasses)
+
+    // Add active class before element start transition and maitain it during the entire transition.
+    addClasses(element, activeClasses)
+    await nextAnimationFrame();
+
+    // remove the initial class on frame after the beginning of the transition
+    removeClasses(element, initialClasses)
+
+    // add the endDlass on frame after the beginning of the transition
+    addClasses(element, endClasses);
+
+    // dynamically comput the duration of the transition from the style of the element
     await afterTransition(element);
 
-    element.classList.remove(...initialClass);
+    // remove both activeClasses and endClasses
+    removeClasses(element, activeClasses)
+    if (removeEndClasses) {
+      removeClasses(element, endClasses)
+    }
+
+    // restore the overlaping classes
+    addClasses(element, stashedClasses)
   }
 
   function initialState() {
@@ -118,16 +168,28 @@ export const useTransition = (controller: TransitionComposableController, option
     }
   }
 
+  function addClasses(element: Element, classes: string[]) {
+    if (classes.length > 0) {
+      element.classList.add(...classes);
+    }
+  }
+
+  function removeClasses(element: Element, classes: string[]) {
+    if (classes.length > 0) {
+      element.classList.remove(...classes);
+    }
+  }
+
   initialState()
   Object.assign(controller, { enter, leave, toggleTransition })
   return [enter, leave, toggleTransition]
 }
 
-function getAttribute(name: string, options: TransitionOptions, dataset: DOMStringMap) {
+function getAttribute(name: string, options: TransitionOptions, dataset: DOMStringMap): string[] {
   const datasetName = `transition${name[0].toUpperCase()}${name.substr(1)}`
   const datasetAlpineName = (alpineNames as any)[name]
-  const classes = (options as any)[name] || dataset[datasetName] || dataset[datasetAlpineName] || ""
-  return classes.split(" ")
+  const classes = (options as any)[name] || dataset[datasetName] || dataset[datasetAlpineName] || " "
+  return isEmpty(classes) ? [] : classes.split(" ")
 }
 
 async function afterTransition(element: Element): Promise<number> {
@@ -138,17 +200,20 @@ async function afterTransition(element: Element): Promise<number> {
         .split(",")[0]
         .replace('s', '')
     ) * 1000
-
     setTimeout(() => {
       resolve(duration)
     }, duration)
   });
 }
 
-async function nextFrame() {
+async function nextAnimationFrame() {
   return new Promise(resolve => {
     requestAnimationFrame(() => {
       requestAnimationFrame(resolve);
     });
   });
+}
+
+function isEmpty(str: string): boolean {
+  return (str.length === 0 || !str.trim());
 }
