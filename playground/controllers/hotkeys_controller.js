0f13c291f39c3647e062e4edcb4abc4bfca73d6f
Julian Rubisch <julian@julianrubisch.at>
2021-06-09 10:46:10 +0200

    Refactor playground example

--- playground/controllers/hotkeys_controller.js
+++ playground/controllers/hotkeys_controller.js
@@ -2,11 +2,16 @@ import { Controller } from 'stimulus'
 import { useHotkeys } from 'stimulus-use'
 
 export default class extends Controller {
-  static hotkeys = { '/': 'showOverlay' }
   static targets = ['overlay']
 
   connect() {
-    useHotkeys(this)
+    useHotkeys(this, {
+      hotkeys: {
+        '/': {
+          handler: 'showOverlay'
+        }
+      }
+    })
   }
 
   showOverlay(event) {
