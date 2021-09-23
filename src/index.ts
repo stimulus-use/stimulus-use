f5344a9e93770ad39874d2c26964a5c22aee7553
Guillaume Briday <guillaumebriday@gmail.com>
2021-09-23 14:53:04 +0200

    Removing webpack-helpers from source code

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,3 @@ export { useTransition, TransitionOptions, TransitionController } from './use-tr
 export { useHotkeys } from './use-hotkeys'
 export { useMatchMedia, MatchMediaOptions } from './use-match-media/index'
 export { useWindowFocus, WindowFocusController } from './use-window-focus/index'
-export { definitionsFromContext } from './webpack-helpers'
