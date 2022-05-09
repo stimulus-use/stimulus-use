ccef0d39bc430e8790143efecc6d99ea8ef53e9e
Marco Roth <marco.roth@intergga.ch>
2022-05-09 15:35:23 -0700

    explictly set vite server port

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080
+  },
   resolve: {
     alias: {
       'stimulus-use': path.resolve(__dirname, '../dist/index.js')
