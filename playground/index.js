471ea15220bb180e54f9cb538fa41c420416e504
Marco Roth <marco.roth@intergga.ch>
2022-05-09 17:17:23 -0700

    Upgrade `stimulus-use` playground (#194)
    * Upgrade Playground
    
    Use Vite
    
    Upgrade Tailwind
    
    Extract Stimulus `application.js`
    
    Expose Stimulus Application on `window`
    
    Removed some dependencies from main `package.json`
    
    * explictly set vite server port
    
    * also set vite server host explicitly
    
    * Upgrade Cypress
    
    * prepend vite commands with `yarn`
    
    * install playground deps

--- playground/index.js
+++ playground/index.js
@@ -1,14 +1,3 @@
-import 'intersection-observer'
-import { Application } from '@hotwired/stimulus'
-import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers'
-import { IntersectionController } from 'stimulus-use'
-
-const application = Application.start()
-const context = require.context('./controllers', true, /\.js$/)
-application.load(definitionsFromContext(context))
-application.register('intersection', IntersectionController)
-
-// enable StimulusUse debug mode
-application.stimulusUseDebug = process.env.NODE_ENV === 'development'
-
 import './index.css'
+import 'intersection-observer'
+import './controllers'
