83fe9f55043e8d3a85ce26f0ec56989da2ae0ebf
Leon Vogt <nonick@nonick.ch>
2022-10-27 21:59:01 +0300

    Update deprecated Stimulus 1 target-syntax (#231)

--- cypress/integration/use-window-resize.spec.js
+++ cypress/integration/use-window-resize.spec.js
@@ -5,29 +5,29 @@ context('useWindowResize', () => {
   })
 
   it('should show the correct window dimensions on initial load', () => {
-    cy.get('[data-target="window-size.width"]').contains('1000')
-    cy.get('[data-target="window-size.height"]').contains('600')
+    cy.get('[data-window-size-target="width"]').contains('1000')
+    cy.get('[data-window-size-target="height"]').contains('600')
   })
 
   it('should adjust the dimensions if the window gets resized', () => {
-    cy.get('[data-target="window-size.width"]').contains('1000')
-    cy.get('[data-target="window-size.height"]').contains('600')
+    cy.get('[data-window-size-target="width"]').contains('1000')
+    cy.get('[data-window-size-target="height"]').contains('600')
 
     cy.viewport(1200, 700)
 
-    cy.get('[data-target="window-size.width"]').contains('1200')
-    cy.get('[data-target="window-size.height"]').contains('700')
+    cy.get('[data-window-size-target="width"]').contains('1200')
+    cy.get('[data-window-size-target="height"]').contains('700')
   })
 
   it('should adjust the dimensions if the window gets resized a lot', () => {
-    cy.get('[data-target="window-size.width"]').contains('1000')
-    cy.get('[data-target="window-size.height"]').contains('600')
+    cy.get('[data-window-size-target="width"]').contains('1000')
+    cy.get('[data-window-size-target="height"]').contains('600')
 
     for (let i = 1; i <= 250; i++) {
       cy.viewport(1000 + i, 600 + i)
     }
 
-    cy.get('[data-target="window-size.width"]').contains('1250')
-    cy.get('[data-target="window-size.height"]').contains('850')
+    cy.get('[data-window-size-target="width"]').contains('1250')
+    cy.get('[data-window-size-target="height"]').contains('850')
   })
 })
