c5cecc53db40c3806137e49399494f7748233108
Marco Roth <marco.roth@intergga.ch>
2023-01-25 16:49:48 +0100

    Chrome -> ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -200,7 +200,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
